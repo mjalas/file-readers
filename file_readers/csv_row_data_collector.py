26b9afd78fc3354ede3e6b9323ee5ff1c6e795b7
Mats Jalas <mats.jalas@gmail.com>
2016-04-04 23:42:34 +0300

    Added header attribute. Added docstrings

--- file_readers/csv_row_data_collector.py
+++ file_readers/csv_row_data_collector.py
@@ -3,7 +3,7 @@ from file_readers.data_collector import BaseDataCollector
 
 class CSVRowDataCollector(BaseDataCollector):
 
-    def __init__(self, data_structure_dict):
+    def __init__(self, data_structure_dict, header=None):
         """The constructor takes the data sources structure as parameter.
 
         Args:
@@ -14,8 +14,10 @@ class CSVRowDataCollector(BaseDataCollector):
                         {'header': 'age', 'index': 1, 'type': int},
                         {'header': 'title', 'index': 2, 'type': str}
                     ]
+            header: The header of the first column.
         """
         self.data_structure = data_structure_dict
+        self.header = header
         self.data = []
 
     def collect_data(self, source):
@@ -23,6 +25,8 @@ class CSVRowDataCollector(BaseDataCollector):
 
         The data source must be a list containing the data.
         """
+        if self.header and self.header in source[0]:  # This header check might be misleading
+            return
         values = {}
         for item in self.data_structure:
             header = item['header']
