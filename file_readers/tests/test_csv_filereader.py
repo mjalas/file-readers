bb7c0b3f61f5f1f2ea6c9f8e741a5e7e9e4eab81
Mats Jalas <mats.jalas@gmail.com>
2016-04-04 23:44:14 +0300

    Added object-oriented way of using the CSVFileReader class.
    Decided to inject a subclass of BaseDataCollector to enable reuse of the CSVFileReader for different implementations of data collection.

--- file_readers/tests/test_csv_filereader.py
+++ file_readers/tests/test_csv_filereader.py
@@ -1,20 +1,47 @@
 from unittest import TestCase
 from file_readers.csv_filereader import CSVFileReader
+from file_readers.data_collector import BaseDataCollector
+
+
+class DummyDataCollector(BaseDataCollector):
+    def __init__(self):
+        self.data_structure = [
+            {'header': 'name', 'index': 0, 'type': str},
+            {'header': 'age', 'index': 1, 'type': int},
+            {'header': 'title', 'index': 2, 'type': str}
+        ]
+        self.data = []
+
+    def collect_data(self, source):
+        values = {}
+        for item in self.data_structure:
+            header = item['header']
+            index = item['index']
+            values[header] = source[index]
+        self.data.append(values)
 
 
 def data_collector(data, row):
-    if 'personnel' not in data:
-        data['personnel'] = []
     values = {'name': row[0], 'age': int(row[1]), 'title': row[2]}
-    data['personnel'].append(values)
+    data.append(values)
 
 
 class TestCSVFileReader(TestCase):
 
     def test_read_file(self):
         test_file = "data/test.csv"
-        data = CSVFileReader.read_file(test_file, func=data_collector, header='Name', delimiter=";")
-        row = data['personnel'][0]
+        collector = DummyDataCollector()
+        reader = CSVFileReader(data_collector=collector, delimiter=";")
+        reader.read_file(test_file)
+        row = collector.data[1]
+        self.assertEqual(row['name'], 'John')
+        self.assertEqual(int(row['age']), 24)
+        self.assertEqual(row['title'], 'Developer')
+
+    def test_read_data_from_file(self):
+        test_file = "data/test.csv"
+        data = CSVFileReader.read_data_from_file(test_file, func=data_collector, header='Name', delimiter=";")
+        row = data[0]
         self.assertEqual(row['name'], 'John')
         self.assertEqual(row['age'], 24)
         self.assertEqual(row['title'], 'Developer')
