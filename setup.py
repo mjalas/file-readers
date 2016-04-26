3bad82ba121874766759c83292e8126082ea9683
Mats Jalas <mjalas@users.noreply.github.com>
2016-04-26 19:21:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
     ],
